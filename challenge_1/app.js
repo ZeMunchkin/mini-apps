0c195b8b6d555e9df3f59206e1115e4dc79caea3
Sara Brandes <smbrandes17@gmail.com>
2018-02-12 15:11:49 -0800

    cleaned up code, added minor styling

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,3 @@
-
 var Board = function () {
 	this.board = {
 		r0: [null, null, null],
@@ -15,9 +14,7 @@ var currentGame = new Board();
 Board.prototype.winByRow = function () {
 	var curGame = this.board
 	for (var key in curGame) {
-		//if all values of array are equal and not null
 		if (curGame[key][0] !== null && curGame[key][0] === curGame[key][1] && curGame[key][0] === curGame[key][2]){
-			//return that value
 			return curGame[key][0];
 		}
 	}
@@ -26,35 +23,26 @@ Board.prototype.winByRow = function () {
 
 Board.prototype.winByColumn = function () {
 	var curGame = this.board;
-	//loop through column indices
 	for (var i = 0; i < 3; i++) {
-		//if value in each row at column are the same and they don't equal null
 		if (curGame.r0[i] !== null && curGame.r0[i] === curGame.r1[i] && curGame.r0[i] === curGame.r2[i]) {
-			//return that value
 			return curGame.r0[i];
 		}
 	}
-	// return null;
 	return null;
 }
 
 Board.prototype.winByDiagonal = function () {
 	var curGame = this.board;
-	//check if values of 0-0, 1-1, and 2-2 are equal & not null
 	if (curGame.r0[0] !== null && curGame.r0[0] === curGame.r1[1] && curGame.r0[0] === curGame.r2[2]) {
-		//if both true return that value
 		return curGame.r0[0];
 
-	//else check if 0-2, 1-1, 2-0 are all equal & not null
 	} else if (curGame.r0[2] !== null && curGame.r0[2] === curGame.r1[1] && curGame.r0[2] === curGame.r2[0]) {
-		//if yes return that value
 		return curGame.r0[2];
-		
 	}
-	//return null
 	return null;
 }
 
+//functions to check for completed games
 Board.prototype.checkForWin = function () {
 	if (this.winByRow()) {
 		return this.winByRow();
@@ -79,7 +67,6 @@ Board.prototype.checkForStalemate = function () {
 	return true;
 }
 
-//function to check for completed games
 Board.prototype.checkForGameOver = function () {
 	if (this.checkForWin()) {
 		return this.checkForWin()
@@ -92,26 +79,17 @@ Board.prototype.checkForGameOver = function () {
 
 
 
-//model
-
-//write a function to handle board clicks, takes a div id argument
+// function to handle model changes
 Board.prototype.place = function (divId) {
 	var curRow = divId.slice(0, 2);
 	var curCol = divId.slice(3);
 	var curGame = this.board;
 
-	//is the boardplace of that div null?
-	//if yes
 	if (!curGame[curRow][curCol]) {
-		// check whose turn it is
-		//if turn is false
 		if (!this.turn) {
-			//value of boardplace is now X
 			curGame[curRow][curCol] = 'X';
 
-		// if turn is true
 		} else {
-			//value of boardplace is now O
 			curGame[curRow][curCol] = 'O';
 		}
 		this.turn = !this.turn;
@@ -122,32 +100,22 @@ Board.prototype.place = function (divId) {
 
 	
 
-
-//view
-
-//function that takes a div id and changes the value of the div
-
-//function to render when game is over
+//Controllers and Views
 var handleGameOver = function (winner) {
 	console.log('invoked');
 	console.log(document.getElementById('gameOver'));
 	if (winner === 'X') {
-		//change div to show that x won
 		document.getElementById('gameOver').innerText = 'Player X has won!';
-		//display div
+
 	} else if (winner === 'O') {
-		//change div to show that o won
 		document.getElementById('gameOver').innerText = 'Player O has won!';
-		//display div
+
 	} else {
-		//change div to stalemate
 		document.getElementById('gameOver').innerText = 'Stalemate!';
-		//display div
 	}
 }
 
 
-//controller
 var handleBoardClick = function (event) {
 	console.log('click!');
 	if (!currentGame.gameOver) {
@@ -179,5 +147,3 @@ var handleNewGame = function () {
 
 }
 
-
-//DOM
