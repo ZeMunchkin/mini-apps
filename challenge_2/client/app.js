cd4e824dd692eb17d6af1813df19d3981f88d6de
Sara Brandes <smbrandes17@gmail.com>
2018-02-13 11:47:27 -0800

    have server up, have html served, and responding to get requests

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
 
 
+var handleClick = function (event) {
+  console.log('I was clicked!');
+
+  var formVal = document.getElementById('csvSubmission').value;
+  console.log(formVal);
+
+  $.ajax({
+    url: '/',
+    method: 'POST',
+    headers: {
+      "content-type": 'application/json',
+    },
+    data: JSON.stringify({
+      "csvSubmission": formVal,
+    }),
+    success: function (data) {
+      console.log('Success!', data);
+    },
+    error: function (data) {
+      console.log('Error!', data);
+    },
+  });
+
+}
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,38 @@
+var express = require('express');
+var parse = require('body-parser');
+var fs = require('fs');
 
+var app = express();
+
+app.use(parse.json());
+
+app.get('/', function (req, res) {
+  console.log('index get!');
+  fs.readFile('./client/index.html', function (err, result) {
+    if (err) {
+      res.sendStatus(404);
+    }
+    res.set('content-type', 'text/html');
+    res.send(result);
+  });
+});
+
+app.get('/app.js', function (req, res) {
+  console.log('app get!');
+  fs.readFile('./client/app.js', function (err, result) {
+    if (err) {
+      res.sendStatus(404);
+    }
+    res.set('content-type', 'application/json');
+    res.send(result);
+  });
+});
+
+app.post('/', function (req, res) {
+  console.log('post!');
+  console.log('req body:', req.body);
+
+});
+
+app.listen(3000);
 
